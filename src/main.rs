84acbe5ba49c43ed572a1d15e83a7578d74fd865
Eduard Mandy <mandyedi@gmail.com>
2024-11-12 18:02:28 +0100

    Read a blob object

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-#[allow(unused_imports)]
 use std::env;
-#[allow(unused_imports)]
 use std::fs;
+use std::io::Read;
 use std::path::Path;
+use flate2::read::ZlibDecoder;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,6 +14,7 @@ fn main() {
 
     match args[1].as_str() {
         "init" => init(),
+        "cat-file" => cat_file(&args),
         _ => print_help(),
     }
 }
@@ -31,8 +32,30 @@ fn init() {
     }
 }
 
+fn cat_file(args: &Vec<String>) {
+    if args.len() < 4 {
+        println!("usage: git cat-file -p <blob_sha>");
+        return;
+    }
+
+    if args[2] == "-p" {
+        let blob_sha= &args[3];
+        let id = &blob_sha[..2];
+        let blob_file_name = &blob_sha[2..];
+
+        let blob = fs::read(format!(".git/objects/{}/{}", id, blob_file_name)).unwrap();
+        let mut decompressed_content = ZlibDecoder::new(&blob[..]);
+        let mut content_string = String::new();
+        decompressed_content.read_to_string(&mut content_string).unwrap();
+        
+        let content = content_string.split('\0').nth(1).unwrap_or("default");
+        print!("{content}");
+    }
+}
+
 fn print_help() {
     println!("usage: git <command>");
     println!("Git commands to use:\n");
-    println!("init    Create an empty Git repository.");
+    println!("init                      Create an empty Git repository.");
+    println!("cat-file -p <blob_sha>    Print blob content.")
 }

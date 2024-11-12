ee7b745dc4a4b652254468d31dfb18787bdbf823
Eduard Mandy <mandyedi@gmail.com>
2024-11-12 16:19:14 +0100

    Refactor: extact init to function and help print [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,37 @@
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
+use std::path::Path;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    eprintln!("Logs from your program will appear here!");
-
     let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
+
+    if args.len() == 1 {
+        print_help();
+        return;
+    }
+
+    match args[1].as_str() {
+        "init" => init(),
+        _ => print_help(),
+    }
+}
+
+fn init() {
+    if !Path::new(".git").is_dir() {
         fs::create_dir(".git").unwrap();
         fs::create_dir(".git/objects").unwrap();
         fs::create_dir(".git/refs").unwrap();
         fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-        println!("Initialized git directory")
-    } else {
-        println!("unknown command: {}", args[1])
+        println!("Initialized git directory.")
     }
+    else {
+        println!("Git repository already initialized.");
+    }
+}
+
+fn print_help() {
+    println!("usage: git <command>");
+    println!("Git commands to use:\n");
+    println!("init    Create an empty Git repository.");
 }

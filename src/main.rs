a465756b41d77cfb6204a6daae45ebf9e5a421ed
Eduard Mandy <mandyedi@gmail.com>
2024-11-12 19:24:45 +0100

    Create a blob object

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::env;
 use std::fs;
 use std::io::Read;
+use std::io::Write;
 use std::path::Path;
-use flate2::read::ZlibDecoder;
+use flate2::{Compression, {read::ZlibDecoder, write::ZlibEncoder}};
+use sha1::{Sha1, Digest};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,6 +17,7 @@ fn main() {
     match args[1].as_str() {
         "init" => init(),
         "cat-file" => cat_file(&args),
+        "hash-object" => hash_object(&args),
         _ => print_help(),
     }
 }
@@ -48,14 +51,45 @@ fn cat_file(args: &Vec<String>) {
         let mut content_string = String::new();
         decompressed_content.read_to_string(&mut content_string).unwrap();
         
-        let content = content_string.split('\0').nth(1).unwrap_or("default");
+        let content = content_string.split('\0').nth(1).unwrap_or("");
         print!("{content}");
     }
+}
+
+fn hash_object(args: &Vec<String>) {
+    if args.len() < 4 {
+        println!("usage: git hash-object -w <file>");
+        return;
+    }
+
+    // read text file
+    let file_content = fs::read_to_string(&args[3]).unwrap_or("".to_string());
+
+    // create blob
+    let length = file_content.len();
+    let blob = format!("blob {}\0{}", length, file_content);
+
+    // hash blob and print
+    let hasher = Sha1::new_with_prefix(&blob);
+    let hash = format!("{:x}", hasher.finalize());
+    print!("{hash}");
+
+    // zip blob
+    let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
+    encoder.write_all(blob.as_bytes()).unwrap();
+    let compressed_blob = encoder.finish().unwrap();
+
+    // write blob to file
+    let folder_name = &hash[..2];
+    let remaining = &hash[2..];
+    fs::create_dir(format!(".git/objects/{}", folder_name)).unwrap();
+    fs::write(format!(".git/objects/{}/{}", folder_name, remaining), compressed_blob).unwrap();
 }
 
 fn print_help() {
     println!("usage: git <command>");
     println!("Git commands to use:\n");
     println!("init                      Create an empty Git repository.");
-    println!("cat-file -p <blob_sha>    Print blob content.")
+    println!("cat-file -p <blob_sha>    Print blob content.");
+    println!("hash-object -w <file>     Computes the SHA hash of <file> and writes it to blob.")
 }
